a8b2f1084a1b87ce7217c3539cd2ff6a4cb299b9
Roman Grmela <romasg1@seznam.cz>
2025-07-26 20:34:36 +0200

    Created NetBeans project

---
+++ GUI_experiment/src/gui_experiment/GUI_experiment.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gui_experiment;
+
+/**
+ *
+ * @author Roman Grmela <romasg1@seznam.cz>
+ */
+public class GUI_experiment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
