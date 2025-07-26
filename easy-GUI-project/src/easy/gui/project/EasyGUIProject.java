f0a8feeee4220c1d5ddc5696a0593cf36fb2f772
Roman Grmela <romasg1@seznam.cz>
2025-07-26 20:29:01 +0200

    Just made java project

---
+++ easy-GUI-project/src/easy/gui/project/EasyGUIProject.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package easy.gui.project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Roman Grmela <romasg1@seznam.cz>
+ */
+public class EasyGUIProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ahoj");
+    }
+    
+}
